6948f0d03dd33aca3b37fa1332f4ea56ee223190
Katy Harrod <m.catherine.harrod@gmail.com>
2022-08-02 09:19:50 -0600

    Add spec testing

---
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'Tea subscription endpoints' do
+  it 'subscribes user to tea' do
+    user1 = User.create!(first_name: "Billy", last_name: "Bob", email: "BillyBob@gmail", address: "123 sesame street")
+    tea1 = Tea.create!(title: "jasmine tea", description: "relaxing", temperature: 210, brew_time: "5 minutes")
+
+    post api_v1_subscriptions_path(user_id: user1.id, tea_id: tea1.id, title: "surprise box tea subscription", annual_frequency: 6, price: 10)
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+
+
+    expected_response = {
+      "data": {
+        "type": "tea subscription",
+        "attributes": {
+          "user_id": "#{user1.id}",
+          "title": "surprise box tea subscription",
+          "annual_frequency": "6",
+          "price": "10",
+          "status": "active",
+          "tea_information": {
+            "id": "#{tea1.id}"
+            "title": "jasmine tea",
+            "description": "relaxing",
+            "temperature": "210",
+            "brew time": "5 minutes"
+          }
+        }
+      }
+    }
+
+    expect(parsed_response).to eq(expected_response)
+  end
+
+  it 'cancel a subscription' do
+    # must be able to list record for user later
+    # just change the status to inactive, nothing else??
+  end
+end
