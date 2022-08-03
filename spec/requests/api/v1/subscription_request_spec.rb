6046c8f9a8bb831de0794eef06a11fcb712ec203
Katy Harrod <m.catherine.harrod@gmail.com>
2022-08-03 09:26:28 -0600

    Spec testing added for user story 2

--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -14,7 +14,8 @@ describe 'Tea subscription endpoints' do
         attributes: {
           title: "surprise box tea subscription",
           annual_frequency: "6",
-          price: "10"
+          price: "10",
+          status: "active"
         }
       }
     }
@@ -22,7 +23,29 @@ describe 'Tea subscription endpoints' do
   end
 
   it 'cancel a subscription' do
-    # post api_v1_subscriptions_path(user_id: user1.id, tea_id: tea1.id, title: "surprise box tea subscription", annual_frequency: 6, price: 10)
+    user1 = User.create!(first_name: "Billy", last_name: "Bob", email: "BillyBob@gmail", address: "123 sesame street")
+    tea1 = Tea.create!(title: "jasmine tea", description: "relaxing", temperature: 210, brew_time: "5 minutes")
+    subscription = Subscription.create!(tea_id: tea1.id, user_id: user1.id, title: "jasmine tea subscription box", annual_frequency: 6, price: 5, status: "active")
+
+    post api_v1_unsubscribe_path(subscription_id: subscription.id)
+    subscription.reload
+
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    expected_response = {
+      data: {
+        type: "tea subscription",
+        attributes: {
+          title: "jasmine tea subscription box",
+          annual_frequency: 6,
+          price: 5,
+          status: "inactive"
+        }
+      }
+    }
+
+    expect(expected_response).to eq(parsed_response)
+    expect(subscription.status).to eq("inactive")
+
 
     # must be able to list record for user later
     # just change the status to inactive, nothing else??
